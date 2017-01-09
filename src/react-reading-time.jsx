a41f94b6f89457fc7ed995fa86fc78672d52301e
frozegnome <pcaron@att.net>
2017-01-08 20:52:41 -0500

    The NOTE I mentioned in the last commit but forgot to add :neutral_face:

--- src/react-reading-time.jsx
+++ src/react-reading-time.jsx
@@ -3,6 +3,16 @@ import {observer} from 'mobx-react';
 import ReadingTime from './reading-time';
 import DevTool from 'mobx-react-devtools';
 
+/*
+  NOTE: So this whole time, every example I see is constructing their stateless component (es6) as so...
+
+    const MyStatelessComponent = ({prop}) => return <Markup />
+
+    The {prop} value is in curly braces, I think this is where I dun goofed. Theres probably some es6 destructuring happening here
+    that I was totally ignorant of. If im not destructuring properly, than we would end up with the 'undefined' errors I
+    was getting. Since im not really doing that here, ditch the braces for now. Working for now
+*/
+
 const ReactReadingTime = observer((props) => {
   const {text, textColor} = props;
 
